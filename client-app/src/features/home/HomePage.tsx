8c1cbcb73e99660538cd1a6225e8327c7af130bf
ashrafulhaque89 <38048752+ashrafulhaque89@users.noreply.github.com>
2019-10-14 20:17:10 +0100

    Minor bug fixes

--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -1,17 +1,58 @@
 import React, { useContext, Fragment } from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
 import { Container, Segment, Header, Button, Image } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 import { RootStoreContext } from '../../app/stores/rootStore';
 import LoginForm from '../user/LoginForm';
 import RegisterForm from '../user/RegisterForm';
 
-const HomePage = () => {
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    height: '100vh',
+  },
+  image: {
+    backgroundImage: 'url(https://source.unsplash.com/featured/?nature)',
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+  },
+  paper: {
+    margin: theme.spacing(8, 4),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+export default function SignInSide() {
   const rootStore = useContext(RootStoreContext);
   const { user, isLoggedIn } = rootStore.userStore;
   const {openModal} = rootStore.modalStore;
+  const classes = useStyles();
+
   return (
-    <Segment inverted textAlign='center' vertical className='masthead'>
+    <Grid container component="main" className={classes.root}>
+      <CssBaseline />
+      <Grid item xs={false} sm={4} md={7} className={classes.image} />
+      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+      <Segment inverted textAlign='center' vertical className='masthead'>
       <Container text>
+      <Header as='h2' inverted content={`Get Active on saving the Planet Earth!`} />
         <Header as='h1' inverted>
           <Image
             size='massive'
@@ -41,7 +82,7 @@ const HomePage = () => {
         )}
       </Container>
     </Segment>
+      </Grid>
+    </Grid>
   );
-};
-
-export default HomePage;
+}
